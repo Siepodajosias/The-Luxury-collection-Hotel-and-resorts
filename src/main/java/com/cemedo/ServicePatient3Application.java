9ead82f5edac4839838e1d273ee8d4c756fed2cd
Siepodajosias <88053473+Siepodajosias@users.noreply.github.com>
2022-06-03 14:17:52 +0000

    configuration du controller

--- src/main/java/com/cemedo/ServicePatient3Application.java
+++ src/main/java/com/cemedo/ServicePatient3Application.java
@@ -2,7 +2,9 @@ package com.cemedo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServicePatient3Application {
 
